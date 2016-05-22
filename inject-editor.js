f1c18e1facd402a6877bf89abf765d0866621940
Tom Elam <tomelam@gmail.com>
2016-05-23 02:44:37 +0530

    Calls the client's callback function with an object containing just the node with accepted changes and the changes assigned to suitably named properties of the object. The subtext string is still not being handled properly.

--- inject-editor.js
+++ inject-editor.js
@@ -4,6 +4,17 @@ require(["dojo/domReady!"], function() {
     });
 });
 
+callback = function(changedStrings) {
+    var key;
+    console.log("Entering callback function");
+    console.group("changedStrings");
+    console.dir(changedStrings);
+    console.groupEnd();
+    for (key in changedStrings) {
+	console.info("changedStrings[" + key + "] is " + changedStrings[key]);
+    }
+}
+
 require(["dojo/keys", "dojo/dom", "dojo/query", "dojo/on", "zen/inline-editor", "dojo/domReady!"],
 	function(keys, dom, query, on, inlineEditor) {
 	    console.log("Setting up the ESCAPE handler (based upon a keydown-event handler)");
@@ -12,7 +23,7 @@ require(["dojo/keys", "dojo/dom", "dojo/query", "dojo/on", "zen/inline-editor", 
 		console.log("Key " + charOrCode);
 		if (charOrCode === keys.ESCAPE) {
 		    console.log("ESCAPE");
-		    inlineEditor.createDialog();
+		    inlineEditor.createDialog(callback);
 		};
 	    });
 	    
--- zen/inline-editor.js
+++ zen/inline-editor.js
@@ -1,15 +1,27 @@
-define(["dojo/mouse", "dojo/dom", "dijit/Dialog", "dijit/form/Button", "dojo/dom-construct", "dojo/on", "dojo/dom-class", "dojo/query"],
+//FIXME: Replace the verbose code below with functions described at
+// https://dojotoolkit.org/reference-guide/1.10/dojo/NodeList-traverse.html ,
+// https://dojotoolkit.org/reference-guide/1.10/dojo/NodeList-manipulate.html , etc.
+//FIXME: Better use Dojo's selector engines. See https://dojotoolkit.org/reference-guide/1.10/dojo/query.html .
+//FIXME: Study JSONQuery for utility. See https://dojotoolkit.org/reference-guide/1.10/dojox/json/query.html .
+//FIXME: Study JSONPath for utility. See https://dojotoolkit.org/api/?qs=1.10/dojox/jsonPath/ .
+//FIXME: When, if ever, should we call the 'focus' method of the Dialog?
+//FIXME: Consider using dijit/InlineEditBox. See https://dojotoolkit.org/reference-guide/1.10/dijit/InlineEditBox.html .
 
-       function(mouse, dom, Dialog, Button, domConstruct, on, domClass, query) {
+define(["dojo/mouse", "dojo/dom", "dijit/Dialog", "dijit/form/Button", "dojo/dom-construct", "dojo/on", "dojo/dom-class",
+	"dojo/query", "dojo/_base/array", "dojo/NodeList-traverse"],
+
+       function(mouse, dom, Dialog, Button, domConstruct, on, domClass, query, array) {
 	   ile = function() {};
-	   var dynDialog, editor, acceptButton, closeButton, clickedOnNode;
+	   var zenPicSlotCount = 0, initialEditableStrings = [], changedStrings = [], callback,
+	       dynDialog, editor, acceptButton, closeButton, clickedOnZenPicSlot;
 
-	   console.log("Defining docEventHandler to use the capture phase of event processing (but not attaching it yet)");
+	   console.log("Define docEventHandler to use the capture phase of event processing (but don't attach it yet)");
 	   ile.docEventHandler = function(event) {
 	       var event = event || window.event;
 	       var target = event.target || event.srcElement;
 	       console.log("Doc click on " + target + ", with ID " +
 			   target.id + ", with target.className '" + target.className + "'");
+	       //FIXME: Zen should not know about class demo-pic-img.
 	       if (domClass.contains(target, "demo-pic-img")) {
 		   ile.showDialog(target);
 	       }
@@ -19,30 +31,59 @@ define(["dojo/mouse", "dojo/dom", "dijit/Dialog", "dijit/form/Button", "dojo/dom
 	       }
 	   };
 
-	   ile.textNodeToText = function(textNode) {
+	   ile.getTextNodeText = function(textNode) {
 	       return (textNode.textContent ? textNode.textContent : textNode.innerText);
 	   };
+
+	   ile.setTextNodeText = function(textNode, text) {
+	       if (textNode.textContent) {
+		   textNode.textContent = text;
+	       } else {
+		   textNode.innerText = text;
+	       }
+	   };
 	   
-	   ile.createDialog = function() {
+	   ile.dir = function(string, object) {
+	       console.group(string);
+	       array.forEach(object, function(item) { console.dir(item); });
+	       console.groupEnd();
+	   };
+
+	   //FIXME: This function is probably too long. Consider breaking it up.
+	   ile.createDialog = function(callback) {
+	       callback = callback || function() { console.info("No callback defined"); }
 	       console.info("Creating Dialog (and leaving it hidden)");
 
-	       console.log("Add docEventHandler to the document node to handle all clicks (TODO:  all events, not just clicks)");
+	       //FIXME: Handle all events, not just clicks.
+	       console.log("Add docEventHandler to the document node to handle all clicks");
 	       document.addEventListener('click', ile.docEventHandler, true);
+
+	       console.log("Save the initial state of every editable string");
+	       query(".zen-pic-slot").forEach(function(node) {
+		   var values = {};
+		   values.node = node;
+		   values.src = node.childNodes[1].childNodes[1].src;
+		   values.figcaption = node.childNodes[1].innerText;
+		   values.subtext = ile.getTextNodeText(node.childNodes[2]);
+		   initialEditableStrings[zenPicSlotCount] = values;
+		   zenPicSlotCount += 1;
+	       });
+	       ile.dir("initial editable strings", [ initialEditableStrings ]);
 
 	       if (!(dynDialog = dijit.byId("dynDialog"))) {
 		   var tree, html = '<div id="editorDialog" style="display: none;">';
 		   html += '<strong>Image URL</strong>';
-		   html += '<textarea id="dialogImageURL" style="width: 90%" placeholder="enter text here"></textarea>';
+		   html += '<textarea id="dialogImageURL" style="width: 90%" placeholder="enter text"></textarea>';
 		   html += '<strong>Caption</strong>';
-		   html += '<textarea id="dialogFigureCaption" style="width: 90%" placeholder="enter text here"></textarea>';
+		   html += '<textarea id="dialogFigureCaption" style="width: 90%" placeholder="enter text"></textarea>';
 		   html += '<strong>Subtext</strong>';
-		   html += '<textarea id="dialogSubtext" style="width: 90%" placeholder="enter text here"></textarea>';
+		   html += '<textarea id="dialogSubtext" style="width: 90%" placeholder="enter text"></textarea>';
 		   html += '<br><div id="editButton"></div><div id="editCloseButton"></div>';
 		   tree = domConstruct.toDom(html);
-		   console.log("Constructing and placing the HTML elements for the Dialog");
+		   console.log("Construct and place the HTML elements for the Dialog");
 		   domConstruct.place(tree, "editorDialogContainer");
 
-		   console.log("Turning the HTML elements into a Dialog");
+		   console.log("Turn the HTML elements into a Dialog");
 		   var pane = dom.byId('editorDialog');
 		   // should specify a width on dialogs with <p> tags, otherwise they get too wide
 		   dynDialog = new dijit.Dialog({
@@ -56,16 +97,70 @@ define(["dojo/mouse", "dojo/dom", "dijit/Dialog", "dijit/form/Button", "dojo/dom
 		   acceptButton = new Button({
 		       label: "Accept",
 		       onClick: function(){
+			   //FIXME: If the user provides an invalid URL, the Dialog cannot be opened again. Fix this!
+			   var event = event || window.event;
+			   var target = event.target || event.srcElement;
+			   var changedStrings = {}, textNode;
+			   console.info("Accept button clicked");
+			   ile.dir("editable nodes of the clicked-on zen-pic-slot",
+				   [ clickedOnImgNode,                             // img
+				     clickedOnImgNode.parentNode,                  // figure
+				     clickedOnImgNode.parentNode.parentNode ]);    // div
+			   array.some(initialEditableStrings,
+				      function(initialZenPicSlotStrings) {
+					  ile.dir("initialZenPicSlotStrings", [ initialZenPicSlotStrings ]);
+					  if (initialZenPicSlotStrings.node.childNodes[1].childNodes[1] ===
+					      clickedOnImgNode) {
+					      ile.dir("Initial strings in the clicked-on zen-pic-slot",
+						      [ initialZenPicSlotStrings ]);
+					      initialStrings = initialZenPicSlotStrings;
+					      return true;
+					  }
+					  else {
+					      console.error("Could not determine the target node");
+					      return false;
+					  }
+				      });
+			   ile.dir("clickedOnImgNode before change", [ clickedOnImgNode,
+								       clickedOnImgNode.parentNode,
+								       clickedOnImgNode.parentNode.parentNode ]);
 			   newVal = dom.byId("dialogImageURL").value;
-			   console.log("Setting image source to " + newVal);
-			   clickedOnNode.src = newVal;
+			   clickedOnImgNode.src = newVal;
 			   newVal = dom.byId("dialogFigureCaption").value;
-			   clickedOnNode.parentNode.childNodes[3].textContent = newVal;
+			   ile.setTextNodeText(clickedOnImgNode.parentNode.childNodes[3], newVal);
 			   newVal = dom.byId("dialogSubtext").value;
-			   //dom.byId('pic-slot-1').childNodes[2].textContent
-			   dom.byId('pic-slot-1').childNodes[2].textContent = newVal;
-			   //clickedOnNode.parentNode.childNodes[2] = newVal;
-			   //FIXME: Is there a 'focus' method to call?
+			   console.debug("New subtext is " + newVal);
+			   textNode = clickedOnImgNode.parentNode.parentNode.childNodes[2];
+			   ile.setTextNodeText(textNode, newVal);
+
+			   ile.dir("clickedOnImgNode after change", [ clickedOnImgNode,
+								      clickedOnImgNode.parentNode,
+								      clickedOnImgNode.parentNode.parentNode ]);
+			   
+			   console.debug("Find the strings that have been changed");
+			   ile.dir("initialStrings", [ initialStrings ]);
+			   changedStrings.node = clickedOnImgNode;
+		       	   if (clickedOnImgNode.src != initialStrings.src) {
+			       changedStrings.src = initialStrings.src;
+			   }
+			   string = ile.getTextNodeText(clickedOnImgNode.parentNode.childNodes[3]);
+			   if (string != initialStrings.figcaption) {
+			       changedStrings.figcaption = string;
+			   }
+			   console.debug("Here 2");
+			   textNode = clickedOnImgNode.parentNode.parentNode.textContent;
+			   string = ile.getTextNodeText(textNode);
+			   if (string != initialStrings.subtext) {
+			       changedStrings.subtext = string;
+			   }
+			   console.debug("Here 3");
+			   ile.dir("changedStrings", [ changedStrings ]);
+
+			   // Call the callback function here instead of in the closeDialog function because
+			   // the user might want a fine-grained persistence mechanism. For example, he might
+			   // not close the Dialog for a long time -- just watching the changes and getting
+			   // everything perfect.
+			   callback(changedStrings);
 		       }
 		   }, "editButton").startup();
 		   
@@ -77,20 +172,18 @@ define(["dojo/mouse", "dojo/dom", "dijit/Dialog", "dijit/form/Button", "dojo/dom
 		       }
 		   }, "editCloseButton").startup();
 
-		   console.log("Adding the 'zen-control' class to certain nodes of the Buttons to allow click events to pass to them");
+		   console.log("Add the 'zen-control' class to certain nodes of the Buttons to allow click events to");
+		   console.log("pass to them");
 		   domClass.add("editButton_label", "zen-control");
 		   domClass.add("editCloseButton_label", "zen-control");
+		   //FIXME: Looks like this query will return too many nodes.
 		   query("input").forEach(function(node) {
 		       domClass.add(node, "zen-control");
 		   });
 
 		   acceptButton = dijit.byId("editButton"), editCloseButton = dijit.byId("editCloseButton");
-		   console.group("acceptButton");
-		   console.dir(acceptButton);
-		   console.groupEnd();
-		   console.group("editCloseButton");
-		   console.dir(editCloseButton);
-		   console.groupEnd();
+		   ile.dir("acceptButton", [ acceptButton ]);
+		   ile.dir("editCloseButton", [ editCloseButton ]);
 	       }
 	       alert("This Zen-enabled web page is now in Edit Mode.");
 	   };
@@ -99,7 +192,7 @@ define(["dojo/mouse", "dojo/dom", "dijit/Dialog", "dijit/form/Button", "dojo/dom
 	       dynDialog = dijit.byId("dynDialog");
 	       acceptButton = dijit.byId("editButton");
 	       closeButton = dijit.byId("editCloseButton");
-	       console.info("destroying dialog");
+	       console.info("destroy Dialog");
 	       console.log("acceptButton => " + acceptButton + ", closeButton => " + closeButton);
 	       acceptButton.destroy();
 	       console.log("Destroyed acceptButton? " + dijit.byId("editButton"));
@@ -112,20 +205,15 @@ define(["dojo/mouse", "dojo/dom", "dijit/Dialog", "dijit/form/Button", "dojo/dom
 	   
 	   ile.showDialog = function(node) {
 	       var textNode, subtext, figureCaption;
-	       console.log("Showing the Dialog");
-	       console.group("editable nodes of the clicked-on demo-pic-slot");
-	       console.dir(node);
-	       console.dir(node.parentNode);
-	       console.dir(node.parentNode.parentNode);
-	       console.groupEnd();
-	       clickedOnNode = node;
+	       console.log("Show the Dialog");
+	       //ile.dir("editable nodes of the clicked-on zen-pic-slot",
+	       //        [ node, node.parentNode, node.parentNode.parentNode ]);
 	       dom.byId("dialogImageURL").value = node.src;
-	       //dom.byId("dialogFigureCaption").value = node.parentNode.innerText;
 	       textNode = node.parentNode.parentNode.childNodes[2];
-	       figureCaption = ile.textNodeToText(node.parentNode.childNodes[3]);
+	       figureCaption = ile.getTextNodeText(node.parentNode.childNodes[3]);
 	       dom.byId("dialogFigureCaption").value = figureCaption;
-	       subtext = textNode.textContent ? textNode.textContent : textNode.innerText;
-	       dom.byId("dialogSubtext").value = subtext.trim();
+	       dom.byId("dialogSubtext").value = ile.getTextNodeText(textNode);
+	       clickedOnImgNode = node; // Store a reference to the clicked-on img node.
 	       dynDialog.show();
 	   };
 	  
